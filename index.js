03635f7ec1e6d6616ccb00ac145eb7a81af6a14e
Asadbek0905 <asadbekurozmatov965@gmail.com>
2023-06-27 18:02:48 +0500

    fix

--- index.js
+++ index.js
@@ -4,18 +4,33 @@ let btn = document.querySelector(".btn")
 let btn1 = document.querySelector(".btn1")
 let p = document.querySelector(".p")
 
+btn.onclick = () => {
+    function totalnum(a) {
+        let value = parseInt(h1.innerText) + a;
+        h1.innerText = value;
+        console.log(value);
+        if(value % 2 == 0){
+            console.log("juft");
+            p.innerText = value + " juft"
 
+        }else{
+            console.log("toq");
+            p.innerText = value + " toq"
+        }
+    }
+    totalnum(1)
+}
 
 btn1.onclick = () => {
     function totalnum(a) {
         let value = parseInt(h1.innerText) - 1;
         h1.innerText = value;
         console.log(value);
-        if (value % 2 == 0) {
+        if(value % 2 == 0){
             console.log("juft");
             p.innerText = value + " juft"
 
-        } else {
+        }else{
             console.log("toq");
             p.innerText = value + " toq"
         }
@@ -26,39 +41,4 @@ btn1.onclick = () => {
 
 
 
-window.addEventListener("keyup", (e) => {
-    if (e.key === "Enter") {
-        function totalnum(a) {
-            let value = parseInt(h1.innerText) + a;
-            h1.innerText = value;
-            console.log(value);
-            if (value % 2 == 0) {
-                console.log("juft");
-                p.innerText = value + " juft"
 
-            } else {
-                console.log("toq");
-                p.innerText = value + " toq"
-            }
-        }
-        totalnum(1)
-    } else {
-        btn.onclick = () => {
-            function totalnum(a) {
-                let value = parseInt(h1.innerText) + a;
-                h1.innerText = value;
-                console.log(value);
-                if (value % 2 == 0) {
-                    console.log("juft");
-                    p.innerText = value + " juft"
-
-                } else {
-                    console.log("toq");
-                    p.innerText = value + " toq"
-                }
-            }
-            totalnum(1)
-        }
-    }
-
-})
